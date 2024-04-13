0b2b08ca4ffd53b5c7787aecda9f2ca313bf18c5
Maxime Moreillon <m.moreillon@gmail.com>
2024-04-13 19:35:35 +0900

    basic auth support

--- src/routes/charts/+page.server.ts
+++ src/routes/charts/+page.server.ts
@@ -1,7 +1,23 @@
-import { env } from '$env/dynamic/public';
+import { env as publicEnv } from '$env/dynamic/public';
+import { env as privateEnv } from '$env/dynamic/private';
 
-export async function load({ fetch, params }) {
-	const res = await fetch(`${env.PUBLIC_CHARTMUSEUM_URL}/api/charts`);
+const { PUBLIC_CHARTMUSEUM_URL } = publicEnv;
+const { CHARTMUSEUM_BASIC_AUTH_USERNAME, CHARTMUSEUM_BASIC_AUTH_PASSWORD } = privateEnv;
+
+export async function load({ fetch }) {
+	const url = `${PUBLIC_CHARTMUSEUM_URL}/api/charts`;
+	const options = {};
+	if (CHARTMUSEUM_BASIC_AUTH_USERNAME && CHARTMUSEUM_BASIC_AUTH_PASSWORD) {
+		const headers = new Headers();
+		headers.append(
+			'Authorization',
+			'Basic ' +
+				Buffer.from(
+					CHARTMUSEUM_BASIC_AUTH_USERNAME + ':' + CHARTMUSEUM_BASIC_AUTH_PASSWORD
+				).toString('base64')
+		);
+	}
+	const res = await fetch(url, options);
 	const charts = await res.json();
 	return { charts };
 }
--- src/routes/charts/[chartName]/+page.server.ts
+++ src/routes/charts/[chartName]/+page.server.ts
@@ -1,8 +1,25 @@
-import { env } from '$env/dynamic/public';
+import { env as publicEnv } from '$env/dynamic/public';
+import { env as privateEnv } from '$env/dynamic/private';
+
+const { PUBLIC_CHARTMUSEUM_URL } = publicEnv;
+const { CHARTMUSEUM_BASIC_AUTH_USERNAME, CHARTMUSEUM_BASIC_AUTH_PASSWORD } = privateEnv;
 
 export async function load({ fetch, params }) {
 	const { chartName } = params;
-	const res = await fetch(`${env.PUBLIC_CHARTMUSEUM_URL}/api/charts/${chartName}`);
+	const url = `${PUBLIC_CHARTMUSEUM_URL}/api/charts/${chartName}`;
+	const options = {};
+	if (CHARTMUSEUM_BASIC_AUTH_USERNAME && CHARTMUSEUM_BASIC_AUTH_PASSWORD) {
+		const headers = new Headers();
+		headers.append(
+			'Authorization',
+			'Basic ' +
+				Buffer.from(
+					CHARTMUSEUM_BASIC_AUTH_USERNAME + ':' + CHARTMUSEUM_BASIC_AUTH_PASSWORD
+				).toString('base64')
+		);
+	}
+
+	const res = await fetch(url, options);
 	const chart = await res.json();
 	return { chart };
 }
