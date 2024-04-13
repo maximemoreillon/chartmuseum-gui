f00c91ee63846f78e934d495d5ddde434ee88129
Maxime Moreillon <m.moreillon@gmail.com>
2024-04-14 07:31:01 +0900

    ewnv

--- src/routes/charts/[chartName]/+page.server.ts
+++ src/routes/charts/[chartName]/+page.server.ts
@@ -2,20 +2,20 @@ import { env as publicEnv } from '$env/dynamic/public';
 import { env as privateEnv } from '$env/dynamic/private';
 
 const { PUBLIC_CHARTMUSEUM_URL } = publicEnv;
-const { CHARTMUSEUM_BASIC_AUTH_USERNAME, CHARTMUSEUM_BASIC_AUTH_PASSWORD } = privateEnv;
+const { CHARTMUSEUM_BASICAUTH_USERNAME, CHARTMUSEUM_BASICAUTH_PASSWORD } = privateEnv;
 
 export async function load({ fetch, params }) {
 	const { chartName } = params;
 	const url = `${PUBLIC_CHARTMUSEUM_URL}/api/charts/${chartName}`;
 	const options = {};
-	if (CHARTMUSEUM_BASIC_AUTH_USERNAME && CHARTMUSEUM_BASIC_AUTH_PASSWORD) {
+	if (CHARTMUSEUM_BASICAUTH_USERNAME && CHARTMUSEUM_BASICAUTH_PASSWORD) {
 		const headers = new Headers();
 		headers.append(
 			'Authorization',
 			'Basic ' +
-				Buffer.from(
-					CHARTMUSEUM_BASIC_AUTH_USERNAME + ':' + CHARTMUSEUM_BASIC_AUTH_PASSWORD
-				).toString('base64')
+				Buffer.from(CHARTMUSEUM_BASICAUTH_USERNAME + ':' + CHARTMUSEUM_BASICAUTH_PASSWORD).toString(
+					'base64'
+				)
 		);
 	}
 
